d055860370bb7fc8b5c0a3fd76178bd51e06905b
andyngojs <dongngo.2000@gmail.com>
2021-07-15 15:43:10 +0700

    feat: add storage data local

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,64 @@
 const APPID = "a6bd6008fdc2e15b4a23cd33828fd8bd";
 const DEFAULT_VALUE = "--";
 
-const searchInput = document.querySelector("#search-input");
-const cityName = document.querySelector(".city-name");
-const weatherState = document.querySelector(".weather-state");
-const weatherIcon = document.querySelector(".weather-icon");
-const temperature = document.querySelector(".temperature");
+const WEATHER_DATA_KEY = "WEATHER_DATA_LOCAL";
 
-const sunrise = document.querySelector(".sunrise");
-const sunset = document.querySelector(".sunset");
-const humidity = document.querySelector(".humidity");
-const windSpeed = document.querySelector(".wind-speed");
+const $ = document.querySelector.bind(document);
+
+const searchInput = $("#search-input");
+const cityName = $(".city-name");
+const weatherState = $(".weather-state");
+const weatherIcon = $(".weather-icon");
+const temperature = $(".temperature");
+
+const sunrise = $(".sunrise");
+const sunset = $(".sunset");
+const humidity = $(".humidity");
+const windSpeed = $(".wind-speed");
 
 searchInput.addEventListener("change", (e) => {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APPID}&units=metric&lang=en`
+    `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APPID}&units=metric&lang=vi`
   ).then(async (res) => {
     const data = await res.json();
     const srcIcon = data.weather[0].icon;
 
     cityName.innerHTML = data.name || DEFAULT_VALUE;
+    setConfig("cityName", data.name);
+
     weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+    setConfig("weatherState", data.weather[0].description);
+
     weatherIcon.setAttribute(
       "src",
       `http://openweathermap.org/img/wn/${srcIcon}@2x.png`
     );
+    setConfig("icon", `http://openweathermap.org/img/wn/${srcIcon}@2x.png`);
+
     temperature.innerHTML = Math.floor(data.main.temp) || DEFAULT_VALUE;
+    setConfig("temp", Math.floor(data.main.temp));
 
     sunrise.innerHTML =
       moment.unix(data.sys.sunrise).format("H:mm") || DEFAULT_VALUE;
+    setConfig("sunrise", moment.unix(data.sys.sunrise).format("H:mm"));
+
     sunset.innerHTML =
       moment.unix(data.sys.sunset).format("H:mm") || DEFAULT_VALUE;
+    setConfig("sunrise", moment.unix(data.sys.sunset).format("H:mm"));
 
     humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
     windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+
+    setConfig("humidity", data.main.humidity);
+    setConfig("windSpeed", data.wind.speed);
   });
 });
+
+const config = JSON.parse(localStorage.getItem(WEATHER_DATA_KEY)) || {};
+
+console.log(config.cityName);
+
+function setConfig(key, value) {
+  config[key] = value;
+  localStorage.setItem(WEATHER_DATA_KEY, JSON.stringify(config));
+}
